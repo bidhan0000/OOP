1eec6416347ecbe5a3e2e088a9b49e4aef009ba7
Sagar Lama <sagarlama0102@gmail.com>
2024-04-11 10:50:39 +0545

    Task3 ans

---
+++ week 1/Task3.java
@@ -0,0 +1,23 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+        /* 3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as "*/
+    System.out.println("A \"quoted\" String is\n'much' better if you learn\nthe rules of \"escape sequences.\"\nAlso,\"\" represent an empty\nString. Don't forget: use \\\ninsted of \" !");
+
+    }
+    
+}
