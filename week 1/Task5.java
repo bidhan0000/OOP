84d9530485910018fb854f6f1071637b12474a8c
Sagar Lama <sagarlama0102@gmail.com>
2024-04-12 19:53:11 +0545

    Task4 ans

---
+++ week 1/Task5.java
@@ -0,0 +1,15 @@
+public class Task5 {
+    /*5. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+********* */
+public static void main(String[] args) {
+    System.out.println("*********\n*********\n*********\n*********");
+}
+    
+}
